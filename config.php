d10f2f69ae2d8d30d34cb539e7c4c3d89affcf30
root <mada@datacom.mw>
2021-02-21 09:40:01 +0000

    added ids file attribute

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ return (object) array(
   'api_pass' => '',
   'gateways_file' => 'json/gateways.json',
   'ip_addr_file' => 'json/ipaddr.json',
+  'entity_ids_file' => 'json/ids.json',
   'pppoe_user_attr' => 'pppoeUsername',
   'pppoe_pass_attr' => 'pppoePassword',
   'pppoe_site_attr' => 'pppoeSiteName',
