a348395a6d7d670d03623d055be0165afeff59ac
Madalitso Mzandu <mada@datacom.mw>
2021-05-22 16:28:50 +0200

    removing unused code

--- index.php
+++ index.php
@@ -3,23 +3,11 @@
 header('content-type: application/json');
 include 'lib/cs_router.php';
 
-function http_response($bool, $message) {
-    $status = 'success';
-    if (!$bool) { // failed response
-        header('X-API-Response: 406', true, 406);
-        $status = 'failed';
-    }
-    return json_encode(
-            array(
-                'status' => $status,
-                'message' => $message,));
-}
-
 $data = false;
 $body = file_get_contents('php://input');
 if ($body) {
     $data = json_decode($body);
 }
-$cs = new CS_Router($data);
-$cs->route();
-echo $cs->http_response();
+$app = new CS_Router($data);
+$app->route();
+echo $app->http_response();
