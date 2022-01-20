f760f3363faefedf485bce0c401c42d051cbca01
Madalitso Mzandu <mada@datacom.mw>
2022-01-20 20:46:05 +0200

    Obsoleting this version

--- src/includes/cors.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
